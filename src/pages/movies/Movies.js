3f72ae87909ed57336ffc90f843eacb99d446054
Qasim-Luqman <qasimluqman7@gmail.com>
2024-05-07 01:10:12 +0500

    Add favButton to MoviesDisplay.js

--- src/pages/movies/Movies.js
+++ src/pages/movies/Movies.js
@@ -2,28 +2,11 @@ import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { fetchMovies } from "../../state/actions";
-import { addDoc, collection } from "firebase/firestore";
-import { db } from "../../config/firebase-config";
-import { getAuth } from "firebase/auth";
 
 const Movies = ({ movies, fetchMovies }) => {
   useEffect(() => {
     fetchMovies();
   }, [fetchMovies]);
-
-  const handleAddToFavorites = async (movie) => {
-    const user = getAuth();
-    try {
-      // Add the movie to the "Favorites" collection in Firebase
-      await addDoc(collection(db, "favourites"), {
-        movie: movie,
-        userId: user.currentUser.uid,
-      });
-      console.log("Movie added to favorites:", movie);
-    } catch (error) {
-      console.error("Error adding movie to favorites:", error);
-    }
-  };
 
   return (
     <div className="movies">
@@ -37,9 +20,6 @@ const Movies = ({ movies, fetchMovies }) => {
           <div className="movie-image">
             <img src={`${movie.poster_link}`} alt="" height={200} width={150} />
           </div>
-          <button onClick={() => handleAddToFavorites(movie)}>
-            Add to Favorites
-          </button>
         </Link>
       ))}
     </div>
--- src/pages/movies/MoviesDetails.js
+++ src/pages/movies/MoviesDetails.js
@@ -1,37 +1,47 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { connect } from "react-redux";
 import { fetchMovie } from "../../state/actions";
 import { useParams } from "react-router-dom";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../../config/firebase-config";
+import { getAuth } from "firebase/auth";
 
 const MovieDetails = ({ movie, fetchMovie }) => {
   const { id } = useParams();
-  const [ isPending, setIsPending ] = useState(false);
 
   useEffect(() => {
-    // setIsPending(true);
     fetchMovie(id);
-    setIsPending(false);
   }, [fetchMovie, id]);
 
-  if (!movie) {
-    return <div>Loading...</div>
-  }
+  const handleAddToFavorites = async (movie) => {
+    const user = getAuth();
+    try {
+      // Add the movie to the "Favorites" collection in Firebase
+      await addDoc(collection(db, "favourites"), {
+        movie: movie,
+        userId: user.currentUser.uid,
+      });
+      console.log("Movie added to favorites:", movie);
+    } catch (error) {
+      console.error("Error adding movie to favorites:", error);
+    }
+  };
 
   return (
     <div>
-      <div className="movie-details">
-        {isPending && <div>Loading...</div>}
-        {!isPending && 
-        <div>
-        <h2><strong>Movie: </strong>{movie.name}</h2>
-        <p><strong>Release Date: </strong>{movie.release_date}</p>
-        <p><strong>Genre: </strong>{movie.genre.join(', ')}</p>
-        <div className="details">
-          <strong>Details: </strong> {movie.description}
+        <div className="movie-details">
+            <div>
+                <h2><strong>Movie: </strong>{movie.name}</h2>
+                <p><strong>Release Date: </strong>{movie.release_date}</p>
+                <p><strong>Genre: </strong>{movie.genre.join(', ')}</p>
+                <div className="details">
+                <strong>Details: </strong> {movie.description}
+                </div>
+            </div>
         </div>
-        </div>
-    }   
-      </div>
+        <button onClick={() => handleAddToFavorites(movie)}>
+            Add to Favorites
+        </button>
     </div>
   );
 };
